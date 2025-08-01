a8008d39aea14f5868ddcff7b4f8646785e5562e
Franco Mayo <francomayo96@gmail.com>
2025-08-01 19:22:25 -0300

    connect frontend to backend

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,22 @@
-import { app, BrowserWindow } from "electron"
+import { app, BrowserWindow, ipcMain } from "electron"
 import path from "path"
+import { isDev } from "./util.js"
+import { getStaticData, pollResources } from "./resourceManager.js"
+import { getPreloadPath } from "./path-resolver.js"
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({})
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  })
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123")
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
+  }
+  pollResources(mainWindow)
+  ipcMain.handle("getStaticData", () => {
+    return getStaticData()
+  })
 })
